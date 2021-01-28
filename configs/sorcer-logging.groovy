83a96a33c2f5c1c689c08dad1760e41b1c9335c8
Dennis Reedy <dennis.reedy@gmail.com>
2021-01-28 11:19:21 -0500

    Check for forceConsoleLogging property

--- configs/sorcer-logging.groovy
+++ configs/sorcer-logging.groovy
@@ -64,7 +64,7 @@ def appenders = []
 /*
  * Only add the CONSOLE appender if we have a console
  */
-if (System.console() != null) {
+if (System.getProperty("forceConsoleLogging") != null || System.console() != null) {
     appender("CONSOLE", ConsoleAppender) {
         if(!System.getProperty("os.name").startsWith("Windows") && System.console() != null) {
             withJansi = true
