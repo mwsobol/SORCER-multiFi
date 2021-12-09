343b4294487f10d373b29247ecf6756ce6e46862
sobolemw <sobol@sorcersoft.org>
2021-12-09 18:51:38 -0500

    support for MatrixEvaluator

--- core/sorcer-platform/src/main/java/sorcer/co/operator.java
+++ core/sorcer-platform/src/main/java/sorcer/co/operator.java
@@ -443,8 +443,12 @@ public class operator extends Operator {
 		return out;
 	}
 
+	public static Tuple2<Integer, Integer> rc(Integer numRows, Integer numCols) {
+		return new Tuple2(numRows, numCols);
+	}
+
 	public static <T1, T2> Tuple2<T1, T2> assoc(T1 x1, T2 x2) {
-		return new Tuple2<T1, T2>(x1, x2);
+		return new Tuple2(x1, x2);
 	}
 
 	public static String path(List<String> attributes) {
